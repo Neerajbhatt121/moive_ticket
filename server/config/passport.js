feaf62b39ec7832598ade4f6ab81532db50a6d0c
Neerajbhatt121 <neerajbhatt1912@gmail.com>
2025-07-18 23:13:56 +0530

    fix: changes for deployment

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/google/callback",
+      callbackURL: "https://moive-ticket-1.onrender.com/api/v1/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log("Google Profile:", profile);
